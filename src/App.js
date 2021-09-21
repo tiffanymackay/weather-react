f491f5004dd6de97e7ae9701784d176ca4f766f8
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-21 09:39:53 -0700

    Add WeatherSearch component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Hello World</h1>
+       <WeatherSearch />
       </header>
     </div>
   );
