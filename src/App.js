a3e0779e8a80a89de6071a6ff0ea13ae17089190
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-21 09:56:01 -0700

    Update footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
        <h1>Weather Application</h1>
        <WeatherSearch />
       </header>
+      <footer>
+      <p>This website is designed and coded by UX Web and Brand Designer <a href="https://www.linkedin.com/in/tiffanylmackay/" target="_blank" rel="noreferrer">Tiffany Mackay</a>, and is <a href="https://github.com/tiffanymackay/weather-react" target="_blank" rel="noreferrer">open-sourced</a>.</p>
+      <small class="text-center">© 2021 Tiffany Mackay</small>
+      </footer>
     </div>
   );
 }
